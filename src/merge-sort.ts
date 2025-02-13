ab6751553405f8b64601eac3cd57cd67c4fd21c9
Akif-Imran <raiden3322@gmail.com>
2025-02-13 17:04:50 +0500

    merge-sort practice

--- src/merge-sort.ts
+++ src/merge-sort.ts
@@ -8,6 +8,49 @@ export const mergeSort = (arr: number[], l: number, r: number): number[] => {
   if (l === r) return arr;
 
   const mid = Math.floor((l + r) / 2);
+
+  mergeSort(arr, l, mid);
+  mergeSort(arr, mid + 1, r);
+
+  merge(arr, l, mid, r);
+
+  return arr;
+};
+
+const merge = (arr: number[], l: number, m: number, r: number) => {
+  const left = arr.slice(l, m + 1);
+  const right = arr.slice(m + 1, r + 1);
+  let i = l,
+    li = 0,
+    ri = 0;
+
+  while (li < left.length && ri < right.length) {
+    if (left[li] <= right[ri]) {
+      arr[i] = left[li];
+      li += 1;
+    } else {
+      arr[i] = right[ri];
+      ri += 1;
+    }
+    i += 1;
+  }
+
+  while (li < left.length) {
+    arr[i] = left[li];
+    li += 1;
+    i += 1;
+  }
+  while (ri < right.length) {
+    arr[i] = right[ri];
+    ri += 1;
+    i += 1;
+  }
+};
+/* export const mergeSort = (arr: number[], l: number, r: number): number[] => {
+  if (arr.length === 0) return arr;
+  if (l === r) return arr;
+
+  const mid = Math.floor((l + r) / 2);
   mergeSort(arr, l, mid);
   mergeSort(arr, mid + 1, r);
 
@@ -40,7 +83,45 @@ const merge = (arr: number[], L: number, M: number, R: number) => {
     ri += 1;
     i += 1;
   }
+}; */
+/* export const mergeSort = (arr: number[], l: number, r: number): number[] => {
+  if (arr.length === 0) return arr;
+  if (l === r) return arr;
+
+  const mid = Math.floor((l + r) / 2);
+  mergeSort(arr, l, mid);
+  mergeSort(arr, mid + 1, r);
+
+  merge(arr, l, mid, r);
+  return arr;
 };
+const merge = (arr: number[], L: number, M: number, R: number) => {
+  const left = arr.slice(L, M + 1);
+  const right = arr.slice(M + 1, R + 1);
+  let i = L,
+    li = 0,
+    ri = 0;
+  while (li < left.length && ri < right.length) {
+    if (left[li] <= right[ri]) {
+      arr[i] = left[li];
+      li += 1;
+    } else {
+      arr[i] = right[ri];
+      ri += 1;
+    }
+    i += 1;
+  }
+  while (li < left.length) {
+    arr[i] = left[li];
+    li += 1;
+    i += 1;
+  }
+  while (ri < right.length) {
+    arr[i] = right[ri];
+    ri += 1;
+    i += 1;
+  }
+}; */
 /* export const mergeSort = (arr: number[], l: number, r: number): number[] => {
   if (arr.length === 0) return arr;
   if (l === r) return arr;
