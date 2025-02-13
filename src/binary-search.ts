5493b6be9b1aa086f87168a973267ecd5a2398db
Akif-Imran <raiden3322@gmail.com>
2025-02-13 17:11:53 +0500

    binary-search practice

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -2,9 +2,28 @@
  * This is binary search standard algo.
  * @param arr arr to search in
  * @param target value to look for
+ * @time O(n log n)
+ * @space O(1)
  * @returns the value index when found otherwise the index where value should be at.
  */
 export const binarySearch = (arr: number[], target: number): number => {
+  if (arr.length === 0) return -1;
+  let l = 0;
+  let r = arr.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return -1;
+};
+
+/* export const binarySearch = (arr: number[], target: number): number => {
   if (arr.length === 0) return -1;
   let l = 0,
     r = arr.length - 1;
@@ -19,7 +38,7 @@ export const binarySearch = (arr: number[], target: number): number => {
     }
   }
   return -1;
-};
+}; */
 /* export const binarySearch = (arr: number[], target: number): number => {
   let l = 0;
   let r = arr.length - 1;
