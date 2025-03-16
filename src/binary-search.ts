e688020e1d2d04bc9554757df1729c15a480d14e
Akif-Imran <raiden3322@gmail.com>
2025-03-16 08:31:48 +0500

    binary search: practice

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -6,7 +6,22 @@
  * @space O(1)
  * @returns the value index when found otherwise the index where value should be at.
  */
-export const binarySearch = (arr: number[], target: number): number => {
+export const binarySearch = (arr: number[], target: number) => {
+  if (arr.length === 0) return -1;
+  let l = 0;
+  let r = arr.length - 1;
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] > target) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return -1;
+};
+/* export const binarySearch = (arr: number[], target: number): number => {
   if (arr.length === 0) return -1;
   let l = 0;
   let r = arr.length - 1;
@@ -21,7 +36,7 @@ export const binarySearch = (arr: number[], target: number): number => {
     }
   }
   return -1;
-};
+}; */
 
 /* export const binarySearch = (arr: number[], target: number): number => {
   if (arr.length === 0) return -1;
