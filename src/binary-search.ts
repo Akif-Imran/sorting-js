149cd999bdef1e0e46465af9dbbfae94060afc4b
Akif-Imran <raiden3322@gmail.com>
2025-03-29 05:29:08 +0500

    binary search: practice

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -12,6 +12,22 @@ export const binarySearch = (arr: number[], target: number) => {
   let r = arr.length - 1;
   while (l <= r) {
     const mid = Math.floor((l + r) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return -1;
+}
+/* export const binarySearch = (arr: number[], target: number) => {
+  if (arr.length === 0) return -1;
+  let l = 0;
+  let r = arr.length - 1;
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
     if (arr[mid] === target) return mid;
     else if (arr[mid] > target) {
       r = mid - 1;
@@ -20,7 +36,7 @@ export const binarySearch = (arr: number[], target: number) => {
     }
   }
   return -1;
-};
+}; */
 /* export const binarySearch = (arr: number[], target: number): number => {
   if (arr.length === 0) return -1;
   let l = 0;
