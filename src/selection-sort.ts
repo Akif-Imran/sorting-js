9efe72c997dbb57eb8633cf16df43b7650aaf5af
Akif-Imran <raiden3322@gmail.com>
2025-03-31 20:47:23 +0500

    selection sort: practice

--- src/selection-sort.ts
+++ src/selection-sort.ts
@@ -1,4 +1,25 @@
+/**
+ * The selection sort algorithm for sorting arrays.
+ * @param arr The array to be sorted
+ * @returns the sorted array
+ * @time O(n^2)
+ * @space O(1)
+ */
 export const selectionSort = (arr: number[]) => {
+  if (arr.length === 0) return arr;
+  const LENGTH = arr.length;
+  for (let i = 0; i < LENGTH - 1; i++) {
+    let selected = i;
+    for (let j = i + 1; j < LENGTH; j++) {
+      if (arr[j] < arr[selected]) {
+        selected = j;
+      }
+    }
+    [arr[selected], arr[i]] = [arr[i], arr[selected]];
+  }
+  return arr;
+};
+/* export const selectionSort = (arr: number[]) => {
   const LENGTH = arr.length;
   for (let i = 0; i < LENGTH - 1; i++) {
     let min = i;
@@ -12,6 +33,6 @@ export const selectionSort = (arr: number[]) => {
     arr[min] = temp;
   }
   return arr;
-};
+}; */
 
 console.log(selectionSort([9, 1, 8, 2, 7, 3, 6, 4, 5]));
